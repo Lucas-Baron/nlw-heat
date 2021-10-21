903764705ad399bc26881eb58a88b550c8ffbfe9
Lucas Baron <lucasbaron28@hotmail.com>
2021-10-21 15:28:57 -0300

    2

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-alert('Olá noob!')
+/*Dados Primitivos:
+    string: 'texto';
+    number: 1234567890;
+    boolean: true and false;
+
+    variáveis: let ou const;
+
+*/
+
+/*const user = {
+  name: 'Lucas',
+  phone: '1234567890',
+  accept: true
+}*/
+
+const links = {
+  github: 'lucas-baron',
+  instagram: 'olucas.baron',
+  facebook: 'lucas.baron.7564'
+}
+
+function changeLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${links[social]}`
+  }
+}
+
+changeLinks()
